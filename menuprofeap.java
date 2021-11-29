5f244065ab0a4daf76b2a5b3d9b67358fc65926d
fernando osorio carmona <49483430+fernandooso@users.noreply.github.com>
2021-11-28 23:17:08 -0300

    Update menuprofeap.java

--- menuprofeap.java
+++ menuprofeap.java
@@ -5,10 +5,63 @@
  */
 package prueba2;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author bodo
  */
 public class menuprofeap {
     
+     public int profeap() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("que desea agregar");
+        System.out.println("1-ver nota: ");
+        ///sacar promedio
+        System.out.println("2-ver anotacion");
+        System.out.println("3-ver asistencia");
+        System.out.println("11-salir");
+        int op = in.nextInt();
+        return op;   
+    }
+     
+     public void vernota() throws SQLException{
+         Scanner in = new Scanner(System.in);
+         System.out.println("ingrese el rut del alumno: ");
+         String rutal= in.nextLine();
+         System.out.println("ingrese codigo de asignatura: ");
+         String codas=in.nextLine(); 
+        conexion con= new conexion();
+        con.conectpsql();
+        String consulta="select * from cursa where estudiante='"+rutal+"' and materia='"+codas+"';";
+        con.consultanotas(consulta);
+        con.desconectar();
+        
+     }
+     
+     public void veranotacion() throws SQLException{
+         Scanner in = new Scanner(System.in);
+         System.out.println("ingrese el rut del alumno: ");
+         String rutal= in.nextLine();
+         conexion con= new conexion();
+        con.conectpsql();
+        String consulta="select * from anotaciones where anotado='"+rutal+"';";
+        con.consultanotaciones(consulta);
+        con.desconectar();
+     }
+    
+    public void verasistencia() throws SQLException{
+        Scanner in = new Scanner(System.in);
+        System.out.println("ingrese el rut del alumno: ");
+        String rutal= in.nextLine();
+        conexion con= new conexion();
+        con.conectpsql();
+        String consulta="select * from asistencia where alumnoa='"+rutal+"';";
+        con.consultasistencia(consulta);
+        con.desconectar();
+        
+     }
+     
+     
 }
